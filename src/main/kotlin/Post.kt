bc751c837217ec82609a08cccd8372760463eb1f
Sergey Nikitin <kexa76@gmail.com>
2021-08-19 19:17:59 +0500

    Revert "Added nullable types"
    This reverts commit 8d36654f193e557f6253b329d7acae4bf09a5968.

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -11,16 +11,12 @@ data class Post(
     val replyOwnerId: Int = 0,
     val replyPostId: Int = 0,
     val friendsOnly: Boolean = false,
-    val comments: Any? = 0,
-    val copyright: Any = 0,
-    val likes: Any? = 0,
-    val reposts: Any? = 0,
+    val comments: Any = 0,
+    val likes: Any = 0,
+    val reposts: Any = 0,
     val views: Any = 0,
     val postType: String = "post",
-    val postSource: Any = 0,
-    val geo: Any? = 0,
     val signerId: Int = 0,
-    val copyHistory: Any? = 0,
     val canPin: Boolean = true,
     val canDelete: Boolean = true,
     val canEdit: Boolean = true,
@@ -28,6 +24,5 @@ data class Post(
     val markedAsAds: Boolean = false,
     val isFavorite: Boolean = false,
     val donut: Any = false,
-    val postponedId: Int = 0,
-    val original: Post?
+    val postponedId: Int = 0
 )
